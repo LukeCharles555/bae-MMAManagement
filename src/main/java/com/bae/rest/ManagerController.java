e456453beecc01c1476cf50fa87c24e83b58010b
qa-admin <qa-admin@qaadmin-HP-ProDesk-400-G3-SFF>
2020-01-13 17:02:04 +0000

    Tweaked backend

--- src/main/java/com/bae/rest/ManagerController.java
+++ src/main/java/com/bae/rest/ManagerController.java
@@ -6,6 +6,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bae.persistance.domain.Fighters;
 import com.bae.persistance.domain.Manager;
 import com.bae.service.ManagerService;
 
@@ -38,11 +40,16 @@ public class ManagerController {
 	
 	@PutMapping("/manager/{managerID}")
 	public Manager updateManager(@PathParam("managerID") Long managerID, @RequestBody Manager manager) {
-		return managerService.updateManager(manager, managerID);
+		return this.managerService.updateManager(manager, managerID);
 	}
 	
 	@DeleteMapping("/manager/{managerID}")
 	public String deleteManager(@PathVariable(value = "managerID") Long managerID) {
 		return managerService.deleteManager(managerID);
+	}
+	
+	@PatchMapping("/update/{managerID}")
+	public Manager addFighterToManager(@PathVariable Long managerID, @RequestBody Fighters fighter) {
+		return this.managerService.addFighterToManager(managerID, fighter);
 	}
 }
